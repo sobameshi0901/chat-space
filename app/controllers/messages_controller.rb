f1578430ebca8a594ce4ac0c9d7f3853357b71f0
Sakurai Shun <sakushun@SakurainoMacBook-Pro.local>
2018-03-13 17:19:30 +0900

    reviese-messages-controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,28 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
     @message = Message.new
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages
-    binding.pry
+    @messages = @group.messages.includes(:user)
   end
 
   def create
     message = Message.new(message_params)
     if message.save
-      redirect_to action: 'index'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
-      render :index, alert: 'メッセージを入力してください'
+      @messages = @group.messages.includes(:user)
+      flash.now[:alert] = 'メッセージを入力してください'
+      render :index
     end
   end
 
   private
   def message_params
     params.require(:message).permit(:content, :image, ).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
