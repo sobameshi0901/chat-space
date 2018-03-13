2f80d16fce9ecd5df671a786551b265fb256a299
Sakurai Shun <sakushun@SakurainoMacBook-Pro.local>
2018-03-13 12:19:21 +0900

    Create-message-create-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    binding.pry
+  end
+
+  def create
+    message = Message.new(message_params)
+    if message.save
+      redirect_to action: 'index'
+    else
+      render :index, alert: 'メッセージを入力してください'
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image, ).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
