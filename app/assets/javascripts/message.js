7ac28c445df76102ae49d5cba15e939373907068
Sakurai Shun <sakushun@SakurainoMacBook-Pro.local>
2018-03-21 13:32:30 +0900

    Add-ajax-for-post-message

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,50 @@
+$(function(){
+  $(document).on('turbolinks:load',function(){
+  function buildHTML(message){
+    var image = ""
+    if(message.image !== null){
+      image = `<img src="${ message.image }", class ="lower-message__image">`
+    }
+    var html = `<div class="chatBox">
+  <div class="chatBox__name">
+    <p>${ message.user_name }</p>
+  </div>
+  <div class="chatBox__date">
+    <p>${ message.message_created }</p>
+  </div>
+  <div class="chatBox__text">
+    <p>${ message.content }</p>
+  </div>
+    ${ image }
+  </div>
+    `
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href
+    console.log(href)
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data);
+      var html = buildHTML(data);
+      $('.chatMain').append(html);
+      $('#message_image').val('');
+      $('#message_content').val('');
+      $('.postBox__button').removeAttr("disabled");
+    })
+    .fail(function(){
+      alert('投稿できませんでした。');
+    })
+  })
+});
+});
